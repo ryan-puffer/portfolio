af5e8b49eab890b4770a816ad5072a5af90fbe8c
ryan-puffer <ryanpufferdev@gmail.com>
2020-09-10 14:00:26 -0500

    add all projects to projectDetails

--- app.js
+++ app.js
@@ -14,25 +14,88 @@ const projectDetails = [
 			'<i class="fab fa-html5"></i><i class="fab fa-css3-alt"></i><i class="fab fa-sass"></i><i class="fab fa-js"></i>',
 		site    : 'https://wheelofsuccessproject.netlify.app',
 		code    : 'https://github.com/ryan-puffer/tdu6-wheel-of-success',
-		details : 'Utilize DOM manipulation and blah blah blah'
+		details :
+			'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas?'
+	},
+	{
+		name    : 'YourApp Dashboard',
+		img     : 'images/thumbnails/WebAppDashboardthumbnail.png',
+		tech    :
+			'<i class="fab fa-html5"></i><i class="fab fa-css3-alt"></i><i class="fab fa-sass"></i><i class="fab fa-js"></i>',
+		site    : 'https://yourappdashboard.netlify.app/',
+		code    : 'https://github.com/ryan-puffer/tdu7-web-app-dashboard',
+		details :
+			'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas?'
+	},
+	{
+		name    : 'PacMan - Desktop Edition',
+		img     : 'images/thumbnails/pacman1thumbnail.png',
+		tech    : '<i class="fab fa-html5"></i><i class="fab fa-css3-alt"></i><i class="fab fa-js"></i>',
+		site    : 'https://pacman-js-game.netlify.app/',
+		code    : 'https://github.com/ryan-puffer/pacman-js',
+		details :
+			'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas?'
+	},
+	{
+		name    : 'YelpCamp',
+		img     : 'images/thumbnails/yelpcamp1thumbnail.png',
+		tech    :
+			'<i class="fab fa-html5"></i><i class="fab fa-css3-alt"><i class="fab fa-node-js"></i></i><i class="fab fa-js"></i>',
+		site    : 'https://yelpcamp-puffer.herokuapp.com/',
+		code    : 'https://github.com/ryan-puffer/yelp_camp/tree/master/YelpCampDeployed',
+		details :
+			'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi non quis exercitationem culpa nesciunt nihil aut nostrum explicabo reprehenderit optio amet ab temporibus asperiores quasi cupiditate. Voluptatum ducimus voluptates voluptas?'
 	}
 ];
 
-//function that executes when a card is clicked on
-function displayModal(card) {
-	//get innerHTML of selected card
-	const modalHTML = `
-    <h1>${projectDetails[0].name}</h1>
-    <img class="modal-thumbnail" src="${projectDetails[0].img}" />
-    <div class="technology">${projectDetails[0].tech}</div>
-    <p class="modal-desc">${projectDetails[0].details}</p>
-    <div class="buttons">
-        <a href="${projectDetails[0].site}" class="card-button">Visit Site</a>
-        <a href="${projectDetails[0].code}" class="card-button">View Code</a>
+window.onload = (e) => {
+	displayProjects();
+};
+
+//displays projects from projectDetails
+function displayProjects() {
+	let projectHTML = '';
+	projectDetails.forEach((project) => {
+		const name = project.name;
+		const img = project.img;
+		const tech = project.tech;
+		const site = project.site;
+		const code = project.code;
+		projectHTML += `
+        <div class="project-thumbnail">
+        <h3>${name}</h3>
+        <img src="${img}" alt="${name}">
+        <div class="icons-card">
+            <p class="field-card">
+                ${tech}
+            </p>
+            <div class="buttons">
+                <a href="${site}" class="card-button">Visit Site</a>
+                <a href="${code}" class="card-button">View Code</a>
+            </div>
+
+        </div>
     </div>
-    `;
-	//set modalHTML to that card
-	modalContent.innerHTML = modalHTML;
+        `;
+	});
+	work.innerHTML = projectHTML;
+}
+
+//function that executes when a card is clicked on
+function displayModal(index) {
+	// //get innerHTML of selected card
+	// const modalHTML = `
+	// <h1>${name}</h1>
+	// <img class="modal-thumbnail" src="${projectDetails[0].img}" />
+	// <div class="technology">${projectDetails[0].tech}</div>
+	// <p class="modal-desc">${projectDetails[0].details}</p>
+	// <div class="buttons">
+	//     <a href="${projectDetails[0].site}" class="card-button">Visit Site</a>
+	//     <a href="${projectDetails[0].code}" class="card-button">View Code</a>
+	// </div>
+	// `;
+	// //set modalHTML to that card
+	// modalContent.innerHTML = modalHTML;
 }
 
 work.addEventListener('click', (e) => {
